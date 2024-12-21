2b66c4f6dd8647715e6186d9c71a6501fd33c086
jljl1337 <43352209+jljl1337@users.noreply.github.com>
2024-12-20 22:38:41 -0700

    refactor: extract repeated logic (#21)
    * extract API content appending logic into a separate function
    
    * rename home function to root for clarity
    
    * update project name, version, and description
    
    * change to packaged application

--- src/testpod/main.py
+++ src/testpod/main.py
@@ -10,29 +10,27 @@ app = Flask(__name__)
 HOSTNAME = socket.gethostname()
 API_URL = os.environ.get('API_URL')
 
-@app.route('/')
-def home():
-    html = f'<h1>Hello, World from {HOSTNAME}!</h1>'
-
+def append_api_content(html: str) -> str:
     if API_URL:
         api_content = requests.get(API_URL).content.decode('utf-8')
         html = f'{html}{api_content}'
 
     return html
+
+@app.route('/')
+def root():
+    return append_api_content(f'<h1>Hello, World from {HOSTNAME}!</h1>')
 
 @app.route('/api')
 def api():
-    html = f'<p>Calling {HOSTNAME}</p>'
+    return append_api_content(f'<p>Calling {HOSTNAME}</p>')
 
-    if API_URL:
-        api_content = requests.get(API_URL).content.decode('utf-8')
-        html = f'{html}{api_content}'
-
-    return html
-
-if __name__ == '__main__':
+def main():
     host = os.environ.get('HOST', '0.0.0.0')
     port = os.environ.get('PORT', 8000)
 
     print(f'Starting server on {host}:{port}')
     serve(app, host=host, port=port)
+
+if __name__ == '__main__':
+    main()
